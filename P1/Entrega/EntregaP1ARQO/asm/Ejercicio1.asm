2aacf94fc7639212413fda40d635cdaec25f82ff
Am0gUsSV2 <roberto.martinalonso@estudiante.uam.es>
2024-10-07 16:24:33 +0200

    Creado zip con la entrega

---
+++ P1/Entrega/EntregaP1ARQO/asm/Ejercicio1.asm
@@ -0,0 +1,77 @@
+# Idea:
+
+# int i
+# int last = -1
+# 
+# for (x in array_size)
+# 	for (elem in array)
+# 		if elem > last (elem-last > 0)
+# 			if elem < i (elem-i < 0)
+# 				i = elem
+# 	
+# 	new_array[x] = i (sw en x*4 de la posicion de new_array)
+# 	last = i (valor minimo)
+# 	i= -1
+#Version 3: A�adimos los nop y cambiamos los la (por hacer, usar el .text para hacer los cambios y meter dos nop en los auipc)
+#Tipo R: Si hay conflicto, dos NOP
+#LW: Si hay conflicto, tres NOP
+#Branch y J: Debido a que se comprueba en ciclo M, aplicamos 3 NOP para que no ejecute ninguna instruccion nueva
+	
+.data
+
+nums: .word 7, 13, 14, 5, 15, 2, 12, 1, 17, 19
+array: .word
+
+.text
+
+	#la s0, nums # Carga la posicion de memoria inicial de la lista de numeros
+	auipc s0,0x0000fc10
+	addi s0,s0,0
+	#la s1, array # Carga la posicion de memoria inicial del array donde vamos a guardar la lista ordenada
+	auipc s1,0x0000fc10 
+	addi s1,s1,0x00000020
+	li a0, 0 # Carga 0 en A0, donde guardaremos el valor del nuevo array (Redundante)
+	li a1, 0 # Inicializa el recorrido del array (Redundante)
+	
+	li s2, 40 # Limite del array (10*4)
+	addi s3, zero, -1 # 0xFFFF, para utilizar para hacer Complemento A1 con XOR
+	addi s4, zero, 1 # Usamos un valor constante de uno para usar BGE en el caso x > 0
+
+	
+	loop_read: #Bucle de lectura
+	
+		add t0, s0, t5 #Calculamos posicion de memoria
+		lw t1, 0(t0) #Cargamos en T1 el valor de nums al que se ha accedido 
+		xor t2, t1, s3 #Complemento A1
+		addi t2, t2, 1 #Complemento A2	
+		add t3, a0, t2 #(A0-T2). Es una comparacion
+		bge t3, zero, loop_advance #Salta si T3 es mayor que A0
+	
+	loop_cmp: #Solo esta para que se sepa que aqui se hace la segunda comparacion
+	
+		beq a2, zero, loop_swapvalue #Si A2 es 0, no hay que comparar.
+		add t4, a2, t2 #Compara si A2 < T3
+		blt t4, zero, loop_advance #Salta si A2 > T3
+	
+	loop_swapvalue: #Solo esta para que se sepa que aqui se cambia el valor
+	
+		add a2, t1, zero #Asignamos el valor en T1 (valor cargado de la lista) a A2 (valor a comparar)	
+	
+	loop_advance: #A�ade "uno" (cuatro) al bucle de lectura y lo continua si aun no ha leido todos los valores
+		addi t5, t5, 4 # Como no podemos usar slli, debemos a�adir 4.
+		bne t5, s2, loop_read # Comparamos con 40
+	
+	loop_write: #Escribe el valor obtenido en el nuevo array, y lo guarda como valor a comparar. Termina si se han escrito todos los valores
+		add t0, s1, a1 #Calcula la direccion de escritura
+		sw a2, 0(t0) #Guarda el valor en la posicion del array correspondiente
+		addi a1, a1, 4 #A�ade "1" (4) al contador de escritura
+		beq a1, s2, end #Comparamos con 40
+		add t5, zero, zero #Resetea el contador de recorrido del array num a 0. En esta seccion, no necesitamos NOP
+		add a0, a2, zero #Asignamos A2 (i) el valor de last (A0)
+		add a2, zero, zero #Reseteamos A2
+		j loop_read #Reiniciamos el programa
+
+	end: #Fin de programa
+	
+	
+	
---
+++ P1/Entrega/EntregaP1ARQO/rtl/hazard_unit.vhd
@@ -0,0 +1,94 @@
+--------------------------------------------------------------------------------
+-- Hazard Unit - Unidad de control de riesgos. ArqO 2024
+-- G.Sutter ago 24.
+--
+-- TODO (Hacer):
+-- Resolver los riesgos de datos (adelantamientos y load-use)
+-- y riesgos de control (Saltos en la etapa MEM)
+-- Este modulo solo tiene las interfaces y genera ceros a las salidas
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;          
+use work.RISCV_pack.all;
+
+
+entity Hazard_unit is
+   port (
+      -- Entradas Forwarding
+      RegWrite_ME : in  std_logic;                     -- Escribir registro en MEM
+      RegWrite_WB : in  std_logic;                     -- Escribir registro en WB
+      RD_ME       : in  std_logic_vector( 4 downto 0); -- Reg escritura en MEM
+      RD_WB       : in  std_logic_vector( 4 downto 0); -- Reg escritura en WB
+      RS1_EX      : in  std_logic_vector( 4 downto 0); -- Reg fuente 1 en EX
+      RS2_EX      : in  std_logic_vector( 4 downto 0); -- Reg fuente 2 en EX
+      -- Entradas para evitar Load-Use
+      MemRead_EX  : in  std_logic;                     -- Instrucción en EX es una Load
+      RD_EX       : in  std_logic_vector( 4 downto 0); -- Reg lectura del Load en EX
+      RS1_ID      : in  std_logic_vector( 4 downto 0); -- Reg fuente 1 en DE
+      RS2_ID      : in  std_logic_vector( 4 downto 0); -- Reg fuente 2 en DE
+
+      -- Salidas Forwarding
+      ForwardA    : out std_logic_vector (1 downto 0); -- Adelantamiento operando 1
+      ForwardB    : out std_logic_vector (1 downto 0); -- Adelantamiento operando 2
+      -- Salidas para evitar Load-USE hazard
+      stall_pipe  : out std_logic                      -- para el pipeline en IF and ID
+   );
+end Hazard_unit;
+
+architecture rtl of Hazard_unit is
+
+begin
+   ForwardA <= "01" when ((unsigned(RS1_EX) = unsigned(RD_ME)) and (RegWrite_ME = '1')) and (unsigned(RS1_EX) /= "00000") else
+               "10" when ((unsigned(RS1_EX) = unsigned(RD_WB)) and (RegWrite_WB  = '1')) and (unsigned(RS1_EX) /= "00000") else
+               "00";
+
+   ForwardB <= "01" when ((unsigned(RS2_EX) = unsigned(RD_ME)) and (RegWrite_ME = '1')) and (unsigned(RS2_EX) /= "00000") else
+               "10" when ((unsigned(RS2_EX) = unsigned(RD_WB)) and (RegWrite_WB  = '1')) and (unsigned(RS2_EX) /= "00000") else
+               "00";
+--Forwarding Unit
+  --process(RS1_EX)
+  --begin
+  -- if((unsigned(RS1_EX) = unsigned(RD_ME)) and RegWrite_ME /= '0') and (unsigned(RS1_EX) /= 0) then
+  --    ForwardA <= "01";
+  -- elsif((unsigned(RS1_EX) = unsigned(RD_WB)) and (RegWrite_WB  /= '0')) and (unsigned(RS1_EX) /= 0) then
+  --    ForwardA <= "10";
+  -- else 
+  --    ForwardA <= "00";
+  -- end if;
+
+  --end process;
+
+ -- process(RS2_EX)
+ -- begin
+ --  if((unsigned(RS2_EX) = unsigned(RD_ME)) and (RegWrite_ME /= '0')) and (unsigned(RS2_EX) /= 0) then
+ --     ForwardB <= "01";
+  -- elsif((unsigned(RS2_EX) = unsigned(RD_WB)) and ((RegWrite_WB) /= '0')) and (unsigned(RS2_EX) /= 0) then
+  --    ForwardB <= "10";
+ --  else 
+ --     ForwardB <= "00";
+ --  end if;
+
+ -- end process;
+
+
+  --Hazard detection unit for LW - Use
+--   stall_pipe <= '1' when (((unsigned(RS1_ID) = unsigned(RD_EX)) or (unsigned(RS2_ID) = unsigned(RD_EX))) and (MemRead_EX = '1')) else
+--                 '0'
+
+--   process(RS1_ID, RS2_ID, RD_EX, MemRead_EX)
+--   begin
+--    if (((unsigned(RS1_ID) = unsigned(RD_EX)) or (unsigned(RS2_ID) = unsigned(RD_EX))) and (MemRead_EX = '1')) then
+--       stall_pipe <= '1';
+--    end if;
+--   end process;
+
+  --stall_pipe <= '0'; -- funcionamiento normal. No detenciones
+
+  stall_pipe <= '1' when (((unsigned(RS1_ID) = unsigned(RD_EX)) or (unsigned(RS2_ID) = unsigned(RD_EX))) and (MemRead_EX = '1')) else
+                '0';
+
+end architecture;
