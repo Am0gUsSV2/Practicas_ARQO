1443449719186e43ad8d8f7227c7b581fb98ca7c
Am0gUsSV2 <roberto.martinalonso@estudiante.uam.es>
2024-10-27 17:29:33 +0100

    Ejercicio 2 terminado

--- P2/Codigo/simple2_intrinsics.c
+++ P2/Codigo/simple2_intrinsics.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <sys/time.h>
 
+#include <immintrin.h>
+
 #define ARRAY_SIZE 2048
-#define NUMBER_OF_TRIALS 7000000
+#define NUMBER_OF_TRIALS 1000000
 
 /*
  * Statically allocate our arrays.  Compilers can
@@ -15,11 +17,21 @@ int main(int argc, char *argv[]) {
 
     struct timeval* t_ini = NULL;
     struct timeval* t_final = NULL;
-    int i,t;
-    double m = 1.0001;
+    int i, ii,t;
+    //double m = 1.0001;
     double t_result_sec = 0;
     double t_result_usec = 0;
     double t_result = 0;
+
+    __m256d mm = {1.0001, 1.0001, 1.0001, 1.0001};
+    __m256d sum = {0.0, 0.0, 0.0, 0.0};
+    __m256d va = {0.0, 0.0, 0.0, 0.0};
+    __m256d vb = {0.0, 0.0, 0.0, 0.0}; 
+    __m256d tmp = {0.0, 0.0, 0.0, 0.0};   
+ 
+    __m256d vib = {-4.0, -3.0, -2.0, -1.0}; 
+    __m256d via = {-3.0, -2.0, -1.0, 0.0}; 
+    __m256d index = {4.0, 4.0, 4.0, 4.0}; 
 
     if (!(t_ini = (struct timeval*)malloc(sizeof (struct timeval)))) {
         printf("Error al crear estructuras de tiempo");
@@ -32,10 +44,17 @@ int main(int argc, char *argv[]) {
         return 1; 
     }
 
+
     /* Populate A and B arrays */
-    for (i=0; i < ARRAY_SIZE; i++) {
-        b[i] = i;
-        a[i] = i+1;
+    for (i=0; i < ARRAY_SIZE; i+= 4) {
+
+        vib = _mm256_add_pd(vib, index);
+        _mm256_store_pd(&b[i], vib);
+        //b[i] = i;
+
+        via = _mm256_add_pd(via, index);
+        _mm256_store_pd(&a[i], via);
+        //a[i] = i+1;
     }
 
     //Medida de rendimiento
@@ -46,12 +65,23 @@ int main(int argc, char *argv[]) {
         printf("Error al obtener tiempo inicial");
         return 1;
     }
+
     /* Perform an operation a number of times */
     for (t=0; t < NUMBER_OF_TRIALS; t++) {
-        for (i=0; i < ARRAY_SIZE; i++) {
-            c += m*a[i] + b[i];
+        /*BUCLE A VECTORIZAR*/
+        for (i=0; i < ARRAY_SIZE; i += 4) {
+            va = _mm256_loadu_pd(&a[i]); //carga 4 dobles (256 bits) desde memoria (no hace falta que esten alineados) y los devuelve
+            vb = _mm256_loadu_pd(&b[i]);
+            tmp = _mm256_fmadd_pd(mm, va, vb); // multiplica datos de precision doble mm y va y el resultado lo suma a los datos en vb y devuelve el resultado
+            sum = _mm256_add_pd(tmp, sum);
         }
     }
+
+    for (ii = 0; ii < 4; ii++) {
+        c += sum[ii];
+    }
+
+
     if (gettimeofday(t_final, NULL) != 0)
     {
         free(t_ini);
